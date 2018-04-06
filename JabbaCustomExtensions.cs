4067fcdab9511a9a8dd318a229c1c54d12265284
Jabba Laci <jabba.laci@gmail.com>
2018-04-06 21:38:35 +0200

    XML comments added

--- JabbaCustomExtensions.cs
+++ JabbaCustomExtensions.cs
@@ -6,14 +6,19 @@ using System.Text;
 
 namespace JabbaCustomExtensions
 {
-    //Extension methods must be defined in a static class
+    /// <summary>
+    /// My own extension methods.
+    /// Extension methods must be defined in a static class.
+    /// </summary>
     public static class CustomExtensions
     {
         // This is the extension method.
         // The first parameter takes the "this" modifier
         // and specifies the type for which the method is defined.
 
-        // capitalize the string (like Python)
+        /// <summary>
+        /// Capitalize the string (like Python).
+        /// </summary>
         public static string Capitalize(this string s)
         {
             if (string.IsNullOrEmpty(s)) return s;
@@ -21,14 +26,16 @@ namespace JabbaCustomExtensions
             return s.Substring(0, 1).ToUpper() + s.Substring(1).ToLower();
         }
 
-        // reverse a string (in Python: s[::-1])
+        /// <summary>
+        /// Reverse a string (like in Python: s[::-1]).
+        /// </summary>
         public static string ReverseStr(this string s)
         {
             return new string(s.Reverse().ToArray());    // requires Linq
         }
 
         /// <summary>
-        /// Get the string slice between the two indexes.
+        /// Get the string slice between the two indexes (like Python).
         /// Inclusive for start index, exclusive for end index.
         /// </summary>
         public static string Slice(this string s, int start, int end)
@@ -58,7 +65,10 @@ namespace JabbaCustomExtensions
             return s.Substring(start, len);    // Return Substring of length
         }
 
-        // slice support for lists (like Python)
+        /// <summary>
+        /// Get the list slice between the two indexes (like Python).
+        /// Inclusive for start index, exclusive for end index.
+        /// </summary>
         public static List<T> Slice<T>(this List<T> li, int start, int end)
         {
             if (start < 0)    // support negative indexing
@@ -85,6 +95,10 @@ namespace JabbaCustomExtensions
             return li.GetRange(start, count);    // return a shallow copy of li of count elements
         }
 
+        /// <summary>
+        /// Take the character n times and concatenate them together to a string.
+        /// Like in Python: `'-' * 20`.
+        /// </summary>
         public static string Times(this char c, int n)
         {
             if (n < 0) return "";
@@ -92,6 +106,10 @@ namespace JabbaCustomExtensions
             return new string(c, n);
         }
 
+        /// <summary>
+        /// Take the string n times and concatenate them together to a string.
+        /// Like in Python: `"-" * 20`.
+        /// </summary>
         public static string Times(this string s, int n)
         {
             var sb = new StringBuilder();
@@ -102,7 +120,14 @@ namespace JabbaCustomExtensions
             return sb.ToString();
         }
 
-        // center a string in a field of given width (like Python's str.center())
+        /// <summary>
+        /// Center a string in a field of given width (like Python's str.center()).
+        /// The string is never truncated.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="width">The returned string is at least this wide.</param>
+        /// <param name="fillChar">Pad the string with this character (default: space).</param>
+        /// <returns>The centered string.</returns>
         public static string Center(this string s, int width, char fillChar=' ')
         {
             var leftMarginWidth = (width - s.Length) / 2;
@@ -112,19 +137,30 @@ namespace JabbaCustomExtensions
                                               fillChar.Times(rightMarginWidth));
         }
 
-        // string to int (like Kotlin)
+        /// <summary>
+        /// Convert the string to int (like Kotlin).
+        /// </summary>
         public static int ToInt(this string s)
         {
             return int.Parse(s);
         }
 
-        // split by whitespaces and remove empty entries (like Python's s.split())
+        /// <summary>
+        /// Split the string by whitespaces and remove the empty entries (like Python's s.split()).
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns>An array of strings containing non-empty parts.</returns>
         public static string[] SplitAndRemoveEmptyEntries(this string s)
         {
             return s.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
         }
 
-        // pretty print an enumerable object, e.g. a list (like Python)
+        /// <summary>
+        /// Pretty print an enumerable object, e.g. a list (like Python).
+        /// Characters are between apostrophes, strings are between quotes.
+        /// Example: ["aa", "bb"]. This output is more readable.
+        /// </summary>
+        /// <returns>A nicely formatted representation of the enumerable object, showing its elements.</returns>
         public static string Pretty<T>(this IEnumerable<T> li)
         {
             var sb = new StringBuilder("[");
@@ -162,10 +198,15 @@ namespace JabbaCustomExtensions
 
     } // end class CustomExtensions
 
-
+    /// <summary>
+    /// Features similar to the Python programming language.
+    /// It contains implementations of Python built-in methods for instance.
+    /// </summary>
     public static class Py
     {
-        // make a range over [start..end) , where end is NOT included (exclusive)
+        /// <summary>
+        /// Make a range over [start..end) , where end is NOT included (exclusive).
+        /// </summary>
         public static IEnumerable<int> RangeExcl(int start, int end)
         {
             if (end <= start) return Enumerable.Empty<int>();
@@ -173,20 +214,33 @@ namespace JabbaCustomExtensions
             return Enumerable.Range(start, end - start);
         }
 
-        // make a range over [start..end] , where end IS included (inclusive)
+        /// <summary>
+        /// Make a range over [start..end] , where end IS included (inclusive).
+        /// </summary>
         public static IEnumerable<int> RangeIncl(int start, int end)
         {
             return RangeExcl(start, end + 1);
         }
 
-        // like Python's input()
+        /// <summary>
+        /// Read a line from the stdin (like Python's input()).
+        /// </summary>
+        /// <param name="prompt">Print this prompt first. Default: empty string.</param>
+        /// <returns>The line provided by the user.</returns>
         public static string Input(string prompt="")
         {
             Console.Write(prompt);
             return Console.ReadLine();
         }
 
-        // Like Python's zip(seq1, seq2). It returns an iterator over tuples.
+        /// <summary>
+        /// Zip the first and second parameters (like Python's zip(seq1, seq2)).
+        /// </summary>
+        /// <typeparam name="TFirst">The first enumerable object.</typeparam>
+        /// <typeparam name="TSecond">The second enumerable object.</typeparam>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns>An iterator that aggregates elements from the two parameters. The elements are grouped in tuples.</returns>
         public static IEnumerable<Tuple<TFirst, TSecond>> Zip<TFirst, TSecond>(
             IEnumerable<TFirst> first,
             IEnumerable<TSecond> second)
--- Math.cs
+++ Math.cs
@@ -2,9 +2,17 @@ using System.Collections.Generic;
 
 namespace JabbaCustomExtensions
 {
-
+    /// <summary>
+    /// Functions to solve mathematical problems.
+    /// </summary>
     public static class Euler
     {
+        /// <summary>
+        /// Find the divisors of a whole number.
+        /// For example: divisors of 6 are 1, 2, 3 and 6.
+        /// </summary>
+        /// <param name="n">The number whose divisors we want to get.</param>
+        /// <returns>A list of integers containing the divisors.</returns>
         public static List<int> Divisors(int n)
         {
             var li = new List<int> {1};
