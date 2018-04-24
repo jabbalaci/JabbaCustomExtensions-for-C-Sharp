bde6781977975e0842a621af2d9e44a4c2c571ac
jabbalaci <jabba.laci@gmail.com>
2018-04-24 14:02:30 +0200

    double.ToInt()

---
+++ JabbaCustomExtensions/DoubleExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JabbaCustomExtensions
+{
+    /// <summary>
+    /// My own double extensions. Overflow exception can occur
+    /// if the double doesn't fit in an int.
+    /// </summary>
+    public static class DoubleExtensions
+    {
+        /// <summary>
+        /// Convert a double to int.
+        /// </summary>
+        public static int ToInt(this double d) =>
+            Convert.ToInt32(d);
+
+    } // end class CharExtensions
+
+}
---
+++ JabbaCustomExtensionsTest/DoubleExtensionsTest.cs
@@ -0,0 +1,21 @@
+﻿using JabbaCustomExtensions;
+using Xunit;
+
+namespace JabbaCustomExtensionsTest
+{
+    /// <summary>
+    /// Tests for double extensions.
+    /// </summary>
+    public class DoubleExtensionsTest
+    {
+        [Fact]
+        public void ToInt()
+        {
+            Assert.Equal(0, 0.0.ToInt());
+            Assert.Equal(12, 12.0.ToInt());
+            Assert.Equal(-12, -12.0.ToInt());
+        }
+
+    } // end class DoubleExtensionsTest
+
+}
